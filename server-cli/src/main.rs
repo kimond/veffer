c6aa16333397a08c552ecaf6c0258804760b691b
Kim Desrosiers <kimdesro@gmail.com>
2018-07-04 08:12:22 -0400

    Add docker-compose.yaml with a simple redis service. Create a basic server using tokio.

--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -1,3 +1,8 @@
+extern crate server;
+
+use server::Server;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new();
+    server.run();
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,7 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate tokio;
+
+use tokio::prelude::*;
+use tokio::io::copy;
+use tokio::net::TcpListener;
+
+pub struct Server {}
+
+impl Server {
+    pub fn new() -> Server {
+        Server {}
+    }
+
+    pub fn run(&self) {
+        let addr = "0.0.0.0:50000".parse().unwrap();
+        let listener = TcpListener::bind(&addr)
+            .expect("unable to bind TCP listener");
+
+        let server = listener.incoming()
+            .map_err(|e| eprintln!("accept failed = {:?}", e))
+            .for_each(|sock| {
+                let (reader, writer) = sock.split();
+                let bytes_copied = copy(reader, writer);
+
+                let handle_conn = bytes_copied.map(|amt| {
+                    println!("wrote {:?} bytes", amt)
+                }).map_err(|err| {
+                    eprintln!("IO error {:?}", err)
+                });
+
+                tokio::spawn(handle_conn)
+            });
+
+        println!("Server running on {}", addr);
+        tokio::run(server);
     }
 }
